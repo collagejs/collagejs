6762845edb8aac643e7c0d2373f52f152b2ead20
José Pablo Ramírez Vargas <webJose@gmail.com>
2026-07-03 23:55:35 -0600

    feat:  Add the noopPiece() function (#34)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { mountPiece } from './mountPiece.js';
 export { mountPieceKey } from './common.js';
 export { ensureGlobalCollageJs } from './global.js';
 export { preventRemount } from './preventRemount.js';
+export { noopPiece } from './noopPiece.js';
